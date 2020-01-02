1a709444827be37084ffe1eeb7f1c5276ecdfa5f
Heath <hushijinde@126.com>
2020-01-02 15:30:34 +0800

    feature: 新增博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,39 +1,94 @@
 module.exports = {
   title: '化简为凡',
   description: '化简为凡的文档， VuePress 文档，技术文档，博客，笔记，心得体会。',
+  head: [
+    ['link', {
+      rel: 'icon',
+      href: '/garageband.ico'
+    }]
+  ],
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
   themeConfig: {
-    nav: [
-      { text: 'External', link: 'https://google.com', target:'_self', rel:'' },
-      { text: 'Guide', link: '/guide/', target:'_blank' },
-      {
-        text: 'Languages',
-        ariaLabel: 'Language Menu',
-        items: [
-          { text: 'Chinese', link: '/language/chinese/' },
-          { text: 'Japanese', link: '/language/japanese/' }
+    repo: 'hushiking/blogs',
+    editLinks: true,
+    lastUpdated: 'Last Updated', // string | boolean
+    nav: [{
+        text: '前端',
+        items: [{
+            text: 'JS',
+            items: [{
+                text: 'JS 语法',
+                link: '/js/base/'
+              },
+              {
+                text: 'JS 框架',
+                link: '/js/framework/'
+              }
+            ]
+          },
+          {
+            text: 'CSS',
+            items: [{
+              text: '布局',
+              link: '/css/layout/'
+            }, {
+              text: 'CSS 语法',
+              link: '/css/base/'
+            }]
+          }
         ]
+      },
+      {
+        text: '后端',
+        link: '/back-end/'
+      },
+      {
+        text: '运维与Linux',
+        link: '/linux/'
+      },
+      {
+        text: '读书笔记',
+        link: '/notes/',
+      },
+      {
+        text: '自由随笔',
+        link: '/essays/'
+      },
+      {
+        text: '关于',
+        link: '/about/'
       }
     ],
     sidebar: {
-      '/css/': [
-        '',     /* /foo/ */
-        'CSS属性中的百分比单位',  /* /foo/one.html */
-        'flex布局左侧固定右侧内容不超过容器'   /* /foo/two.html */
+      '/css/base/': [
+        '', /* /foo/ */
+        'CSS属性中的百分比单位', /* /foo/one.html */
       ],
 
-      '/javascript/': [
-        '',      /* /bar/ */
+      '/css/layout/': [
+        '',
+        'flex布局左侧固定右侧内容不超过容器', /* /foo/two.html */
+      ],
+
+      '/js/base/': [
+        '', /* /bar/ */
         'for...in与for...of区别', /* /bar/three.html */
-        'form表单的name属性与submit()默认事件'   /* /bar/four.html */
+        'form表单的name属性与submit()默认事件', /* /bar/four.html */
+      ],
+
+      '/js/framework/': [
+        '',
       ],
 
       // fallback
       '/': [
-        '',        /* / */
-        'contact', /* /contact.html */
-        'about'    /* /about.html */
+        '', /* / */
+        'about' /* /about.html */
       ]
-    },
-    lastUpdated: 'Last Updated', // string | boolean
+    }
   }
 }
